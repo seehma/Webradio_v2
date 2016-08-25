7b14b70e1bcd6cf08766ae838bee8d1d1bf195f3
Acer54 <11a2ae04>
2016-08-25 22:10:48 +0200

    Fix for "undefined Namespace prefix" in xml response.

--- lib/lastFM_AlbumArtGrabber.py
+++ lib/lastFM_AlbumArtGrabber.py
@@ -1,6 +1,7 @@
 import urllib
 try:
-    import xml.etree.cElementTree as ETree
+    #import xml.etree.cElementTree as ETree
+    from lxml import etree as ETree
 except:
     import xml.etree.ElementTree as ETree
 
@@ -9,7 +10,7 @@ class LastFMDownloader(object):
     def __init__(self, album_name, artist_name):
         """ Initializes LastFM Downloader """
 
-        self.LASTFM_API_KEY = "a42ead6d2dcc2938bec2cda08a03b519" # Please use your OWN LastFM API key
+        self.LASTFM_API_KEY = "a42ead6d2dcc2938bec2cda08a03b519"
         self.LASTFM_URL = "http://ws.audioscrobbler.com/2.0/?method=album.search&album={album_name}&api_key=" + self.LASTFM_API_KEY
         self.album_name = album_name
         self.artist_name = artist_name
@@ -24,7 +25,11 @@ class LastFMDownloader(object):
         """ Use LastFM's API to obtain a URL for the album cover art """
 
         response = urllib.urlopen(self.url).read() # Send HTTP request to LastFM
-        xml_data = ETree.fromstring(response) # Read in XML data
+        #Due to a change in the API, Namespace prefix is not defined an causes Errors! 
+        #Hotfix: Use "lxml" instead of "xml"
+        parser = ETree.XMLParser(recover=True)
+        xml_data = ETree.fromstring(response, parser) # Read in XML data
+        
 
         for element in xml_data.getiterator("album"):
             if (element.find('artist').text.lower() == self.artist_name.lower().encode("utf-8")):
